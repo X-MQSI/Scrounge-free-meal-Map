f8cd31b0be13ba5579afbc14b9b824d3b5795af0
山雨欲来风满楼 <maqingshui@outlook.com>
2024-07-06 17:49:03 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,13 @@
 let students=
 [
     {
-        "name": "测试",
+        "name": "测试1",
+        "school": "海口实验中学",
+        "city": "海口",
+        "province": "海南"
+    },
+    {
+        "name": "测试2",
         "school": "海口实验中学",
         "city": "海口",
         "province": "海南"
