432dd0bb3d9ab00204f7994113c4b60e608ad64c
山雨欲来风满楼 <maqingshui@outlook.com>
2024-07-22 12:58:24 +0800

    Update data.js
    更新

--- js/data.js
+++ js/data.js
@@ -1,15 +1,69 @@
 let students=
 [
     {
-        "name": "测试1",
-        "school": "海口实验中学",
+        "name": "李家成",
+        "school": "武警警官学院",
+        "city": "成都",
+        "province": "四川"
+    },
+    {
+        "name": "马志伟",
+        "school": "中北大学",
+        "city": "太原",
+        "province": "山西"
+    },
+        {
+        "name": "姚诗远",
+        "school": "太原理工大学",
+        "city": "太原",
+        "province": "山西"
+    },
+        {
+        "name": "纪喆",
+        "school": "辽宁石油化工大学",
+        "city": "抚顺",
+        "province": "辽宁"
+    },
+        {
+        "name": "李传乾",
+        "school": "吉林农业科技学院",
+        "city": "吉林",
+        "province": "吉林"
+    },
+        {
+        "name": "陈鸿讳",
+        "school": "海南大学",
         "city": "海口",
         "province": "海南"
     },
-    {
-        "name": "测试2",
-        "school": "海口实验中学",
+        {
+        "name": "林书艺",
+        "school": "海南大学",
         "city": "海口",
         "province": "海南"
-    }
+    },
+        {
+        "name": "王鑫潼",
+        "school": "海南大学",
+        "city": "海口",
+        "province": "海南"
+    },
+        {
+        "name": "韦泽亮",
+        "school": "海南大学",
+        "city": "海口",
+        "province": "海南"
+    },
+        {
+        "name": "黄业添",
+        "school": "遵义医科大学",
+        "city": "遵义",
+        "province": "贵州"
+    },
+        {
+        "name": "庞文慧",
+        "school": "河北北方学院",
+        "city": "张家口",
+        "province": "河北"
+    },
 ]
