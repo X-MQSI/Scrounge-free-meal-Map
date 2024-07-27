7e87923acdeee4a8d8eec2569b4f9164751ad879
山雨欲来风满楼 <maqingshui@outlook.com>
2024-07-27 23:18:32 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,69 +1,314 @@
-let students=
-[
-    {
-        "name": "李家成",
-        "school": "武警警官学院",
-        "city": "成都",
-        "province": "四川"
-    },
-    {
-        "name": "马志伟",
-        "school": "中北大学",
-        "city": "太原",
-        "province": "山西"
-    },
+let students =
+    [
         {
-        "name": "姚诗远",
-        "school": "太原理工大学",
-        "city": "太原",
-        "province": "山西"
-    },
+            "name": "李家成",
+            "school": "武警警官学院",
+            "city": "成都",
+            "province": "四川"
+        },
         {
-        "name": "纪喆",
-        "school": "辽宁石油化工大学",
-        "city": "抚顺",
-        "province": "辽宁"
-    },
+            "name": "马志伟",
+            "school": "中北大学",
+            "city": "太原",
+            "province": "山西"
+        },
         {
-        "name": "李传乾",
-        "school": "吉林农业科技学院",
-        "city": "吉林",
-        "province": "吉林"
-    },
+            "name": "姚诗远",
+            "school": "太原理工大学",
+            "city": "太原",
+            "province": "山西"
+        },
         {
-        "name": "陈鸿讳",
-        "school": "海南大学",
-        "city": "海口",
-        "province": "海南"
-    },
+            "name": "纪喆",
+            "school": "辽宁石油化工大学",
+            "city": "抚顺",
+            "province": "辽宁"
+        },
         {
-        "name": "林书艺",
-        "school": "海南大学",
-        "city": "海口",
-        "province": "海南"
-    },
+            "name": "李传乾",
+            "school": "吉林农业科技学院",
+            "city": "吉林",
+            "province": "吉林"
+        },
         {
-        "name": "王鑫潼",
-        "school": "海南大学",
-        "city": "海口",
-        "province": "海南"
-    },
+            "name": "陈鸿讳",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
         {
-        "name": "韦泽亮",
-        "school": "海南大学",
-        "city": "海口",
-        "province": "海南"
-    },
+            "name": "林书艺",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
         {
-        "name": "黄业添",
-        "school": "遵义医科大学",
-        "city": "遵义",
-        "province": "贵州"
-    },
+            "name": "王鑫潼",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
         {
-        "name": "庞文慧",
-        "school": "河北北方学院",
-        "city": "张家口",
-        "province": "河北"
-    },
-]
+            "name": "韦泽亮",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "黄业添",
+            "school": "遵义医科大学医学与科技学院",
+            "city": "遵义",
+            "province": "贵州"
+        },
+        {
+            "name": "庞文慧",
+            "school": "河北北方学院",
+            "city": "张家口",
+            "province": "河北"
+        },
+        {
+            "name": "瞿悦伊",
+            "school": "景德镇陶瓷大学",
+            "city": "景德镇",
+            "province": "江西"
+        }, {
+            "name": "王若璇",
+            "school": "宁波大学",
+            "city": "宁波",
+            "province": "浙江"
+        },
+        {
+            "name": "向馨",
+            "school": "河北北方学院",
+            "city": "张家口",
+            "province": "河北"
+        },
+        {
+            "name": "杜其恒",
+            "school": "内蒙古医科大学",
+            "city": "呼和浩特",
+            "province": "内蒙古"
+        },
+        {
+            "name": "陈泰儒",
+            "school": "武夷学院",
+            "city": "南平",
+            "province": "福建"
+        },
+        {
+            "name": "张扬",
+            "school": "烟台大学",
+            "city": "烟台",
+            "province": "山东"
+        },
+        {
+            "name": "刘扬民",
+            "school": "广西科技大学",
+            "city": "柳州",
+            "province": "广西"
+        },
+        {
+            "name": "陈端英",
+            "school": "贵州医科大学",
+            "city": "贵阳",
+            "province": "贵州"
+        },
+        {
+            "name": "李传乾",
+            "school": "吉林农业科技学院",
+            "city": "吉林",
+            "province": "吉林"
+        },
+        {
+            "name": "龙仕宝",
+            "school": "湖南文理学院",
+            "city": "常德",
+            "province": "湖南"
+        },
+        {
+            "name": "张裕妹",
+            "school": "湖南理工学院",
+            "city": "岳阳",
+            "province": "湖南"
+        },
+        {
+            "name": "黄基喆",
+            "school": "锦州医科大学",
+            "city": "锦州",
+            "province": "辽宁"
+        },
+        {
+            "name": "杜欣怡",
+            "school": "四川农业大学",
+            "city": "成都",
+            "province": "四川"
+        },
+        {
+            "name": "田金林",
+            "school": "辽宁中医药大学",
+            "city": "沈阳",
+            "province": "辽宁"
+        },
+        {
+            "name": "吕慧恩",
+            "school": "山东建筑大学",
+            "city": "济南",
+            "province": "山东"
+        },
+        {
+            "name": "王秋霞",
+            "school": "河南大学",
+            "city": "郑州",
+            "province": "河南"
+        },
+        {
+            "name": "肖静",
+            "school": "江西理工大学",
+            "city": "南昌",
+            "province": "江西"
+        },
+        {
+            "name": "王娜",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "刘俊池",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "林道永",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "罗清莲",
+            "school": "赣东学院",
+            "city": "抚州",
+            "province": "江西"
+        },
+        {
+            "name": "翁宗弘",
+            "school": "南昌大学",
+            "city": "南昌",
+            "province": "江西"
+        },
+        {
+            "name": "吴靖婷",
+            "school": "江西科技师范大学",
+            "city": "南昌",
+            "province": "江西"
+        },
+        {
+            "name": "符丁升",
+            "school": "海南热带海洋学院",
+            "city": "三亚",
+            "province": "海南"
+        },
+        {
+            "name": "徐业丰",
+            "school": "江西水利电力大学",
+            "city": "南昌",
+            "province": "江西"
+        },
+        {
+            "name": "何景峰",
+            "school": "长春电子科技学院",
+            "city": "长春",
+            "province": "吉林"
+        },
+        {
+            "name": "戴宁",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "王种奕",
+            "school": "南华大学",
+            "city": "衡阳",
+            "province": "湖南"
+        },
+        {
+            "name": "秦以晋",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "王太康",
+            "school": "上海立信会计学院",
+            "city": "杨浦",
+            "province": "上海"
+        },
+        {
+            "name": "胡欣恺",
+            "school": "湖南文理学院",
+            "city": "常德",
+            "province": "湖南"
+        },
+        {
+            "name": "刘雨馨",
+            "school": "南华大学",
+            "city": "衡阳",
+            "province": "湖南"
+        },
+        {
+            "name": "王顺晴",
+            "school": "南阳理工学院",
+            "city": "南阳",
+            "province": "河南"
+        },
+        {
+            "name": "胡健华",
+            "school": "上海工程技术大学",
+            "city": "松江",
+            "province": "上海"
+        },
+        {
+            "name": "贺勋峰",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "林桑宇",
+            "school": "海南大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "黄宇悦",
+            "school": "海南科技职业大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "郑皓夫",
+            "school": "海南科技职业大学",
+            "city": "海口",
+            "province": "海南"
+        },
+        {
+            "name": "蔡琳",
+            "school": "遵义医科大学医学与科技学院",
+            "city": "遵义",
+            "province": "贵州"
+        },
+        {
+            "name": "邝涵诗",
+            "school": "防灾科技学院",
+            "city": "廊坊",
+            "province": "河北"
+        },
+        {
+            "name": "刘云霄",
+            "school": "海南师范大学",
+            "city": "海口",
+            "province": "海南"
+        },
+    ]
